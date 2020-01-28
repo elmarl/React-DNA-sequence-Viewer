e97721079f3d0ffd24c13e32c450cf82184bc1b6
Elmar <elmar.laigna@gmail.com>
2020-01-28 14:21:00 +0200

    basic DNA seq display with row header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Seqedit extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            dnaSeq: "Loremipsumloremipsumaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        }
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    transformDNAseqToHtml(text) {
+        let newtext = [];
+        for (let i = 0; i < text.length; i++) {
+            if (i % 10 === 0 && i % 100 !== 0) {
+                newtext.push(<span>&nbsp;&nbsp;&nbsp;&nbsp;</span>)
+                newtext.push(<span className="DNAletter">{text.charAt(i)}</span>)
+            } else if (i % 100 === 0 && i !== 0) {
+                newtext.push(<br></br>)
+                newtext.push(<span className="DNAletter">{text.charAt(i)}</span>)
+            } else {
+                newtext.push(<span className="DNAletter">{text.charAt(i)}</span>)
+            }
+        }
+        return newtext;
+    }
+
+    generateRowHeader(){
+        let rowCount = Math.ceil(this.state.dnaSeq.length / 100)
+        let rowHeader = [];
+        for (let i = 0; i< rowCount; i++){
+            rowHeader.push(<span>{(i*100+1)}:&nbsp;</span>);
+            rowHeader.push(<br></br>);
+        }
+        return rowHeader;
+    }
+
+    render() {
+        return (
+            <div>
+                <span className="rowHeader">{this.generateRowHeader()}</span>
+                <span className="DNAseq">{this.transformDNAseqToHtml(this.state.dnaSeq)}</span>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Seqedit />, document.getElementById('root'));
---
+++ src/js/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Seqedit from '../.js';
+
+class App extends React.Component {
+    render(){
+        return(<Seqedit />)
+    }
+}
+
+//ReactDOM.render(<App />, document.getElementById('root'));
